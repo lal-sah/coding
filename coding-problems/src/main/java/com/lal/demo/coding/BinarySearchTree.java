98a0693ab08c9195429563436c1774056ddc51a1
lalsah <lalsah@192.168.0.49>
2018-05-12 09:46:38 -0500

    BinarySearchTree

---
+++ coding-problems/src/main/java/com/lal/demo/coding/BinarySearchTree.java
@@ -0,0 +1,23 @@
+package com.lal.demo.coding;
+
+public class BinarySearchTree {
+	public static boolean contains(Node root, int value) {
+		if (root == null) {
+			return false;
+		} else if (root.value == value) {
+			return true;
+		} else if (value < root.value) {
+			return contains(root.left, value);
+		} else {
+			return contains(root.right, value);
+		}
+	}
+
+	public static void main(String[] args) {
+		Node n1 = new Node(1, null, null);
+		Node n3 = new Node(3, null, null);
+		Node n2 = new Node(2, n1, n3);
+
+		System.out.println(contains(n2, 3));
+	}
+}
