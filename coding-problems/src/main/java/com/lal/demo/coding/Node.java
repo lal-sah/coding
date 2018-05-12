fb72d9ca9ce523df236d7494231c3963db3d63f2
lalsah <lalsah@192.168.0.49>
2018-05-12 09:47:05 -0500

    BinarySearchTree

---
+++ coding-problems/src/main/java/com/lal/demo/coding/Node.java
@@ -0,0 +1,12 @@
+package com.lal.demo.coding;
+
+public class Node {
+	public int value;
+	public Node left, right;
+
+	public Node(int value, Node left, Node right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
+}
